41bb43ce4256c7d95da340fb40f8615da5813ef8
george <georgeikeagu@gmail.com>
2022-03-11 09:26:21 +0100

    first commit

---
+++ Guessing game/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+           while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount<guessLimit)
+                {
+                    Console.WriteLine("Enter guess:");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses=true;
+                }   
+            }
+           if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!!");
+            }
+            else
+            {
+                Console.Write("You win!!");
+                
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
